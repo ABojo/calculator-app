8ffe0f9e3c928f96cbe2b02836d71f9eea47aa76
ABojo <Alex.bojorquez2598@gmail.com>
2023-07-31 20:37:44 -0400

    wrote an object that will wrap up all the operator functions and allow them to be accessible using the operator as a key

---
+++ src/utils/math.ts
@@ -0,0 +1,14 @@
+export default {
+  "+": function (a: number, b: number): number {
+    return a + b;
+  },
+  "-": function (a: number, b: number): number {
+    return a - b;
+  },
+  x: function (a: number, b: number): number {
+    return a * b;
+  },
+  "/": function (a: number, b: number): number {
+    return a / b;
+  },
+};
